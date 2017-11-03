67e9debe04a6f51bbc43fbf231afe41714bc6b15
Marius Ursache <marius@marius.me.uk>
2017-11-03 17:07:56 +1100

    Added basics for apikey session redis support

---
+++ middleware/apikey-session-redis.js
@@ -0,0 +1,12 @@
+"use strict";
+
+module.exports = function authByApiKeyRedis(Department, redisClient) {
+  const store = require("../lib/store")(Department, redisClient);
+  const session = require("../lib/session")(store);
+
+  return function authByApiKeyRedisMiddleware(req, res, next) {
+    return session.authByApiKey(req, res, function authByApiKeyCallback(err, department) {
+      return next(err);
+    });
+  };
+};
