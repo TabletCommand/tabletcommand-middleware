e6005bc4c4c73d5f6de020e6265214b1813fc6d6
Marius Ursache <marius@marius.me.uk>
2017-11-06 14:44:48 +1100

    Moved common data setup to separate file

--- test/store.js
+++ test/store.js
@@ -2,7 +2,6 @@
 
 // cSpell:words mockgoose tabletcommand backend
 
-const _ = require("lodash");
 const assert = require("chai").assert;
 
 const mongoose = require("mongoose");
@@ -15,58 +14,17 @@ let mockgoose = new Mockgoose(mongoose);
 const redisClient = require("redis-js");
 
 const store = require("../lib/store")(models.Department, redisClient);
+const data = require("./data")(mockgoose, mongoose, models, redisClient);
 
-const testApiKey = "secretapikey1990";
-const d = {
-  "_id": mongoose.Types.ObjectId(),
-  "signupDomains": [],
-  "signupKey": "abcd",
-  "incidentTypes": [],
-  "rtsAuthKey": "rtsAuthKey123",
-  "rtsChannelPrefix": "rtsChannelPrefix123",
-  "rtsEnabled": true,
-  "pushEnabled": true,
-  "heartbeatEnabled": true,
-  "cadBidirectionalEnabled": true,
-  "cadMonitorMinutes": 30,
-  "cadMonitorEnabled": false,
-  "cadEmailUsername": "",
-  "apikey": testApiKey,
-  "active": true,
-  "modified_unix_date": 1492743603.514,
-  "state": "CO",
-  "city": "Denver",
-  "fdid": "06905",
-  "uuid": "e4c5873c-1684-47a9-ad2d-81ded6e7ac15",
-  "modified_date": "2017-04-21T03:00:03.514"
-};
-let testDepartment = models.Department(d);
-
-const prepareTestData = function prepareTestData(callback) {
-  testDepartment.save().then(function(err, savedItem) {
-    return callback(err);
-  });
-};
+const testApiKey = data.apiKey;
 
 describe("Store", function() {
   beforeEach(function(done) {
-    mockgoose.prepareStorage().then(function() {
-      mongoose.connect("mongodb://127.0.0.1:27017/TestingDB", {
-        useMongoClient: true // this option silents the warning, but does not cleanup the data
-      }, function(err) {
-        prepareTestData(function() {
-          done(err);
-        });
-      });
-    });
+    data.beforeEach(done);
   });
 
   afterEach(function(done) {
-    mockgoose.helper.reset().then(function() {
-      redisClient.flushall(function() {
-        done();
-      });
-    });
+    data.afterEach(done);
   });
 
   it("isMocked", (done) => {
@@ -128,6 +86,5 @@ describe("Store", function() {
         });
       });
     });
-
   });
 });
