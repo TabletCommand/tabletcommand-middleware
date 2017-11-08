417a426cd54caf4edfccccc6f20ab4c0fafc98aa
Marius Ursache <marius@marius.me.uk>
2017-11-08 11:01:35 +1100

    Group session middleware in one object

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 "use strict";
 
-var session = require("./middleware/custom-session");
-var signupSession = require("./middleware/signup-session");
-var metrics = require("./middleware/metrics");
-var tokenSession = require("./middleware/token-session");
+// cSpell:words signup apikey
 
+var customSession = require("./middleware/custom-session");
+var signupSession = require("./middleware/signup-session");
+var tokenSession = require("./middleware/token-session");
+var senecaSession = require("./middleware/seneca-session-redis");
+var apiKeySession = require("./middleware/apikey-session-redis");
+
+var metrics = require("./middleware/metrics");
 var redis = require("./lib/redis");
 var helpers = require("./lib/helpers");
 var routesCommon = require("./routes/common");
 
 module.exports = {
-  session: session,
-  signupSession: signupSession,
+  session: {
+    legacy: customSession,
+    signup: signupSession,
+    token: tokenSession,
+    seneca: senecaSession,
+    apiKey: apiKeySession
+  },
   metrics: metrics,
-  tokenSession: tokenSession,
-
   redis: redis,
   helpers: helpers,
   routesCommon: routesCommon
