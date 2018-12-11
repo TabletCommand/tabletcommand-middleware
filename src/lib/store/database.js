ac4a0cf268ff1dafbb6c423b7ed2df334184f9c3
Marius Ursache <marius@marius.me.uk>
2018-12-11 12:46:36 +1100

    Expose userContributionEnabled

--- src/lib/store/database.js
+++ src/lib/store/database.js
@@ -21,7 +21,8 @@ module.exports = function(Department, Session, User) {
       "cadEmailUsername",
       "apikey",
       "active",
-      "department"
+      "department",
+      "userContributionEnabled"
     ]
   };
 
