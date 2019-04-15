882a6c8f26186a2bbef78f43b6d90f04a6339c39
David Banham <david@banham.id.au>
2019-04-15 14:27:24 +1000

    Add express middleware to log accesses

--- src/lib/bunyan-logger.js
+++ src/lib/bunyan-logger.js
@@ -32,6 +32,27 @@ const logger = function loggerFunc(name, filePath, logToConsole) {
   return logger;
 };
 
+const middleware = function (loggerInstance) {
+  return function accessLogMiddleware(req, res, next) {
+    // This doesn't fire the log immediately, but waits until the response is finished
+    // This means we have a chance of logging the response code
+    res.on("finish", () => {
+      loggerInstance.info({
+        remoteAddress: req.ip,
+        method: req.method,
+        url: req.originalUrl,
+        protocol: req.protocol,
+        hostname: req.hostname,
+        httpVersion: `${req.httpVersionMajor}.${req.httpVersionMinor}`,
+        userAgent: req.headers["user-agent"],
+        status: res._header ? res.statusCode : undefined
+      }, "access_log");
+    });
+    next();
+  };
+};
+
 module.exports = {
-  logger: logger
+  logger: logger,
+  middleware: middleware
 };
